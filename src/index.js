fa6fa76b61cfe860db46ddbfc65e4cde94a1eeb7
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-04-12 03:06:07 +0530

    listen to all incomming ips

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use('/', routes);
 app.use('/auth', authRoutes);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
